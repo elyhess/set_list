0c3fe125db5f6c3221b2d8e42d0d9b1401cf98b7
Ely Hess <ely.hess@me.com>
2020-12-11 09:54:40 -0700

    Add playlist_songs imlpementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_222709) do
+ActiveRecord::Schema.define(version: 2020_12_11_164743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2020_12_02_222709) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "playlist_songs", force: :cascade do |t|
+    t.bigint "song_id"
+    t.bigint "playlist_id"
+    t.index ["playlist_id"], name: "index_playlist_songs_on_playlist_id"
+    t.index ["song_id"], name: "index_playlist_songs_on_song_id"
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -31,5 +42,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_222709) do
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "playlist_songs", "playlists"
+  add_foreign_key "playlist_songs", "songs"
   add_foreign_key "songs", "artists"
 end
